69dd6b837526092501239e19e962ad168c036060
akhil <akhileshreddy9181@gmail.com>
2023-03-03 00:04:42 -0500

    Added Exit Status to script

--- vars/variables.tf
+++ vars/variables.tf
@@ -14,7 +14,19 @@ output "var_from_tfvars_file" {
 
 #variable "input" {}
 
+variable "sample2"{
+  default = "hellooo"
+}
+
 variable "sample3" {
+  default = [
+    "Different",
+    true,
+    false,
+    9875432]
+}
+
+variable "sample4" {
   default = {
     Name = "steve"
     Number = 222
@@ -23,5 +35,5 @@ variable "sample3" {
 }
 
 output "display_steve_info" {
-  value = var.sample3
+  value = "Variables in Sample2 is : ${var.sample2}, Variables in Sample3 is : ${var.sample3[0]}, Variables in Sample4 is : ${var.sample4[Name]}"
 }
