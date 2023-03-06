122b260dde736f476da7a440c3e32907f02565ca
akhil <akhileshreddy9181@gmail.com>
2023-03-06 16:06:31 -0500

    Added Exit Status to script

--- roboshop-setup/example1/servers.tf
+++ roboshop-setup/example1/servers.tf
@@ -7,6 +7,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "frontend-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
@@ -14,6 +22,14 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue"
   }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "catalogue-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.catalogue.private_ip]
 }
 
 resource "aws_instance" "mongodb" {
@@ -25,6 +41,14 @@ resource "aws_instance" "mongodb" {
   }
 }
 
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "mongodb-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mongodb.private_ip]
+}
+
 resource "aws_instance" "user" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
@@ -32,6 +56,14 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "user-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.user.private_ip]
 }
 
 resource "aws_instance" "redis" {
@@ -43,6 +75,14 @@ resource "aws_instance" "redis" {
   }
 }
 
+resource "aws_route53_record" "redis" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "redis-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.redis.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
@@ -50,6 +90,14 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "cart-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -61,6 +109,14 @@ resource "aws_instance" "mysql" {
   }
 }
 
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "mysql-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mysql.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
@@ -68,6 +124,14 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "shipping-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.shipping.private_ip]
 }
 
 resource "aws_instance" "rabbitmq" {
@@ -79,6 +143,14 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "rabbitmq-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
@@ -88,6 +160,14 @@ resource "aws_instance" "payment" {
   }
 }
 
+resource "aws_route53_record" "payment" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "payment-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.payment.private_ip]
+}
+
 resource "aws_instance" "dispatch" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
@@ -95,4 +175,12 @@ resource "aws_instance" "dispatch" {
   tags = {
     Name = "dispatch"
   }
+}
+
+resource "aws_route53_record" "dispatch" {
+  zone_id = "Z00402221E1TOA6JZJNT9"
+  name    = "dispatch-dev.devopsa.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.dispatch.private_ip]
 }
