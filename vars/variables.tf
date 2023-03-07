366a6e1ca004d01c1d1ebd7b47ed684c3e7766f9
akhil <akhileshreddy9181@gmail.com>
2023-03-07 10:35:51 -0500

    Added Exit Status to script

--- vars/variables.tf
+++ vars/variables.tf
@@ -1,39 +1,56 @@
-variable "sample"{
-  default = 234
-}
+#variable "sample"{
+#  default = 234
+#}
+#
+#output "My_Variable_is" {
+#  value = var.sample
+#}
+#
+#variable "sample1" {}
+#
+#output "var_from_tfvars_file" {
+#  value = var.sample1
+#}
+#
+##variable "input" {}
+#
+#variable "sample2"{
+#  default = "hellooo"
+#}
+#
+#variable "sample3" {
+#  default = [
+#    "Different",
+#    true,
+#    false,
+#    9875432]
+#}
+#
+#variable "sample4" {
+#  default = {
+#    Name = "steve"
+#    Number = 222
+#    email = "steve222@gmail.com"
+#  }
+#}
+#
+#output "display_steve_info" {
+#  value = "Variables in Sample2 is : ${var.sample2}, Variables in Sample3 is : ${var.sample3[3]}, Variables in Sample4 is : ${var.sample4["Name"]}"
+#}
 
-output "My_Variable_is" {
-  value = var.sample
-}
-
-variable "sample1" {}
-
-output "var_from_tfvars_file" {
-  value = var.sample1
-}
-
-#variable "input" {}
-
-variable "sample2"{
-  default = "hellooo"
-}
-
-variable "sample3" {
+variable "d"{
   default = [
-    "Different",
-    true,
-    false,
-    9875432]
+    {
+      course_name = "aws",
+      trainer_name = "raghu"
+    },
+    {
+      course_name = "devops",
+      trainer_name = "ramu"
+    }
+  ]
 }
 
-variable "sample4" {
-  default = {
-    Name = "steve"
-    Number = 222
-    email = "steve222@gmail.com"
-  }
-}
-
-output "display_steve_info" {
-  value = "Variables in Sample2 is : ${var.sample2}, Variables in Sample3 is : ${var.sample3[3]}, Variables in Sample4 is : ${var.sample4["Name"]}"
+output "display_course_name" {
+  value = "The course names are : ${var.d.*.course_name}"
 }
