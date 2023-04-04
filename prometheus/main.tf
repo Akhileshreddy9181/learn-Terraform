0673269aaa724e5b1718011e71928b26c82ef17f
akhil <akhileshreddy9181@gmail.com>
2023-04-04 11:13:16 -0400

    Added Exit Status to script

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "node1" {
   ami           = "ami-0e68b80662fefe1c2"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-006c61c287cf5f4d5"]
-  wait_for_fulfillment = true
+  //wait_for_fulfillment = true
 
   tags = {
     Name = "prom-test-server"
@@ -13,7 +13,7 @@ resource "aws_spot_instance_request" "node2" {
   ami           = "ami-0e68b80662fefe1c2"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-006c61c287cf5f4d5"]
-  wait_for_fulfillment = true
+  //wait_for_fulfillment = true
 
   tags = {
     Name = "prom-test-node"
